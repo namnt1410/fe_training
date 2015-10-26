93d206fb98f1ca05061b52b12cf6a1c0f4083818
namnt1410 <namnt.1410@gmail.com>
2015-10-26 13:51:02 +0000

    refs#42 Add detail info for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!, :except => [:home] 
+  
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).concat [:name, :gender, :age]
+    devise_parameter_sanitizer.for(:account_update).concat [:name, :gender, :age]
+  end
 end
