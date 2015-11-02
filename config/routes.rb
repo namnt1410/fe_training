14bae65bc3cc824485589927d2e2865142ac6b0e
namnt1410 <namnt.1410@gmail.com>
2015-11-02 16:27:03 +0000

    add friend basic

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root :to => 'static_pages#home'
 
   devise_for :users
+  resources :users, only: :show
+  resources :friends, only: [:create, :destroy, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
